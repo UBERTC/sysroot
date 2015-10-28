527181ee40f74cc083726dc3c63e9e408d8514e1
Chet Kener <Cl3Kener@gmail.com>
2015-10-28 05:51:24 -0400

    Minor Includes update
    * this change follows the aosp NDK includes change in 6.0.0_r1
    
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- arch-arm/usr/include/math.h
+++ arch-arm/usr/include/math.h
@@ -536,7 +536,7 @@ int	__builtin_ilogb(double) __NDK_FPABI_MATH__ __pure2;
 #if !defined(__clang__) || __clang_major__ > 3 || (__clang_major__ == 3 && __clang_minor__ >= 6)
 int	__builtin_isnan(double) __NDK_FPABI_MATH__ __pure2;
 #else
-/* clang < 3.5 has faulty prototype for __builtin_isnan */
+/* clang < 3.7 has faulty prototype for __builtin_isnan */
 #endif
 double	__builtin_lgamma(double) __NDK_FPABI_MATH__;
 long long __builtin_llrint(double) __NDK_FPABI_MATH__;
